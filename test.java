39caeecf679a1aed999c53a0cb191de387bd2575
yaoyu2001 <36496681+yaoyu2001@users.noreply.github.com>
2018-09-19 02:11:27 +0000

    Add files via upload
    We write these codes together and upload by me. Just because I need to integrate all features.

---
+++ test.java
@@ -0,0 +1,27 @@
+
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		String pathname = "Test_Family_Team wisdom of Mr Toad.ged"; 
+		
+		File filename = new File(pathname);
+        Project03.printINDIAndFAMTables(filename);
+	
+		
+}
+}
+
