0352c6584b346e4b66630157690dda0d600c4d5e
zlaomin <zlaomamin@gmail.com>
2018-10-02 21:56:23 -0400

    Update fuc_z.java
    fix some bugs

--- fuc_z.java
+++ fuc_z.java
@@ -1,18 +1,18 @@
 package cs5551;
-
+import java.util.Vector;
 public class fuc_z {
-	public static void check_male_name(Family f,Individual[] I)//male same name
+	public static void check_male_name(Family f,Vector<Individual> I)//male same name
 	{
 		String name[]=f.getHusbandName().split(" ");
 		String failyname=name[name.length-1];
 		for(int i=0;i<f.childrenId.size();i++)
 		{
 			int num=Integer.parseInt((f.childrenId.get(i)));
-			if(I[num].gender.equals("Male"))
+			if(I.elementAt(num).gender.equals("Male"))
 			{
 				String sonname;
 				String[] namelist;
-				sonname=I[num].getName();
+				sonname=I.elementAt(num).getName();
 				namelist=sonname.split(" ");
 				namelist[namelist.length-1]=failyname;
 				sonname=namelist.toString();
