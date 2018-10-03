ecfd0c9e3d5d64bf2a56d782c62b798ec20d0c92
yaoyu2001 <36496681+yaoyu2001@users.noreply.github.com>
2018-10-02 21:37:44 -0400

    Add files via upload
    This is my user stories and test of it. US30&US31 Yongchang Yao

---
+++ read.java
@@ -0,0 +1,195 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class read {
+	private static List<Individual> allIndividuals = new ArrayList<Individual>();
+	private static List<Family> allFamilies = new ArrayList<Family>();
+
+	public static  void readfile(File f) {
+        BufferedReader br = null;
+
+        try {
+	    	br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+	    	
+	    	Individual indv = null;
+	    	Family currentFamily = null;
+	    	boolean readingBirthDate = false;
+	    	boolean readingDeathDate = false;
+	    	boolean readingMarriageDate = false;
+	    	boolean readingDivorceDate = false;
+	    	// read each line from the file
+	    	for (String line = br.readLine(); line != null; line = br.readLine()) {
+
+				// Split the line into words	
+				String[] words = line.split(" ");
+
+				// check if the line is valid
+				if(words != null && words.length >= 2) {
+					
+					boolean isSpecialScenario = false;
+					if(words.length >= 3 && (words[2].equals("INDI") || words[2].equals("FAM")) ) {
+						isSpecialScenario = true;
+					}
+					
+					// obtain level, tag and value from the line
+					String level = words[0];
+					String tag = "";
+					String value = "";
+					
+					if(!isSpecialScenario) {
+						tag = words[1];
+						for(int i=2;i<words.length;i++) {
+							value += words[i] + " ";
+						}
+					} else {
+						tag = words[2];
+						value = words[1];
+						for(int i=3;i<words.length;i++) {
+							value += " " +words[i];
+						}
+					}
+					
+					// Check if we encounter INDI record, 
+					if(tag.equals("INDI")) {
+						indv = new Individual();
+						indv.setId(value);
+						
+						allIndividuals.add(indv);
+					}
+					
+					if(tag.equals("NAME") && indv != null) {
+						indv.setName(value);
+					}
+					if(tag.equals("SEX")) {
+						indv.setGender(value);
+					}
+					
+					if(tag.equals("BIRT")) {
+						readingBirthDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingBirthDate) {
+						readingBirthDate = false;
+						indv.setBirthDate(value);
+					}
+					
+					if(tag.equals("MARR")) {
+						readingMarriageDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingMarriageDate) {
+						readingMarriageDate = false;
+						currentFamily.setMarriageDate(value);
+					}
+
+					if(tag.equals("DIV")) {
+						readingDivorceDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingDivorceDate) {
+						readingDivorceDate = false;
+						currentFamily.setDivorceDate(value);
+					}
+
+					if(tag.equals("DEAT")) {
+						readingDeathDate = true;
+						continue;
+					}
+					if(tag.equals("DATE") && readingDeathDate) {
+						readingDeathDate = false;
+						indv.setDeathDate(value);
+					}
+					
+					if(tag.equals("FAM")) {
+						currentFamily = null;
+						for (Family family:allFamilies) {
+							if(family.getId().trim().equals((value.replaceAll("@", "")).trim()))
+								currentFamily = family;
+						}
+						if(currentFamily == null) {
+							currentFamily = new Family();
+							currentFamily.setId(value.replaceAll("@", ""));
+							allFamilies.add(currentFamily);
+						}
+					}
+					
+					if(tag.equals("FAMS")) {
+						indv.getSpouseFamilyIds().add(value);
+						
+						// Let's add the spouse to family table
+						Family fam = null;
+						for (Family family:allFamilies) {
+							if(family.getId().equals(value.replaceAll("@", "")))
+								fam = family;
+						}
+						if(fam == null) {
+							fam = new Family();
+							fam.setId(value.replaceAll("@", ""));
+							allFamilies.add(fam);
+						}
+						if(indv.getGender().trim().equals("M")) {
+							fam.setHusbandName(indv.getName());
+							fam.setHusbandId(indv.getId());
+						} else {
+							fam.setWifeName(indv.getName());
+							fam.setWifeId(indv.getId());
+						}
+					}
+					
+					if(tag.equals("FAMC")) {
+						indv.getChildFamilyIds().add(value);
+						
+						// Let's add the children IDs to the family table
+						Family fam = null;
+						for (Family family:allFamilies) {
+							if(family.getId().equals(value.replaceAll("@", "")))
+								fam = family;
+						}
+						if(fam == null) {
+							fam = new Family();
+							fam.setId(value.replaceAll("@", ""));
+							allFamilies.add(fam);
+						}
+						if(fam.getChildrenId() == null) {
+							List<String> childrenIds = new ArrayList<String>();
+							fam.setChildrenId(childrenIds);
+						} else {
+							if(!fam.getChildrenId().contains(value))
+								fam.getChildrenId().add(value);
+						}
+					}
+					
+				}
+	    	}
+	    
+        }  catch (IOException e) {
+        	//e.printStackTrace();
+        	System.out.println("Look like you passed incorrect file name");
+        }  finally {
+        	try {
+        		if (br != null)br.close();
+        		
+        	} catch (IOException ex) {
+        		ex.printStackTrace();
+        	}
+        }
+
+	}
+	public static List<Family> getFamilies(){
+		return allFamilies;
+	}
+	
+	public static List<Individual> getIndividuals(){
+		return allIndividuals;
+	}
+}
