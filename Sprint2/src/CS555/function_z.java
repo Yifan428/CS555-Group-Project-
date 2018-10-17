e43d92085971f8668ae983dd01cc609296139664
zlaomin <zlaomamin@gmail.com>
2018-10-16 21:56:27 -0400

    Update function_z.java

--- Sprint2/src/CS555/function_z.java
+++ Sprint2/src/CS555/function_z.java
@@ -1,5 +1,13 @@
 package CS555;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class function_z {
@@ -20,9 +28,10 @@ public class function_z {
 				sonname=I.get(num).getName();
 				namelist=sonname.split(" ");
 				if(namelist[namelist.length-1]!=failyname){
-					System.out.println("ERROR: FAMILY: US16: The "+f.id+" family's man's last name is not same ");	
+					System.out.print("line "+function_z.findwhichline(namelist[namelist.length-1])+" ");
+					System.out.println("Error: FAMILY: US16: The F"+f.id+" family's man's last name is not same ");	
 					namelist[namelist.length-1]=failyname;
-				} 
+				}
 				sonname="";
 				for(int x=0;x<namelist.length;x++)
 				{
@@ -39,16 +48,57 @@ public class function_z {
 	{
 		if(f.childrenId==null)
 			return;
-		if(f.childrenId.size()>5)
-			System.out.println("ERROR: FAMILY: US14 The "+f.id+" family have more than 5 child");
+		if(f.childrenId.size()>5) {
+			System.out.print("line "+ function_z.findwhichline(f.id));
+			System.out.println("Error: FAMILY: US14 The F"+f.id+" family have more than 5 child");
 	}
+}
 	public static void US_test(List<Family> f, List<Individual> I){
-		//System.out.println("US14:");
-		//System.out.println("Family with more than 5 child in the same time is:");
+		System.out.println("US14:");
+		System.out.println("Family with more than 5 child in the same time is:");
 		for(int i=0;i<f.size();i++)
 			check_num(f.get(i));
-		//System.out.println("US16");
+		System.out.println("US16&US40");
+		System.out.println("Include input line numbers");
+		System.out.println("Same man in fanmily have same last name");
 		for(int i=0;i<f.size();i++)
-			check_male_name(f.get(i),I);  
+			check_male_name(f.get(i),I);
+		System.out.println("US41");
+		System.out.println("Include partial dates");
+		Individual I_test =new Individual();
+		I_test.setBirthDate("1986");
+		I_test.setDeathDate("2018-10");
+		System.out.println("His birthdata and deathData is partial dates"+I_test.getBirthDate()+"     "+I_test.getDeathDate());
 	}
+	
+	public static int findwhichline(String name) {
+		String lineReader = null; 
+		String pathname= "Test_Family_Team wisdom of Mr Toad.ged";
+		BufferedReader br = null;
+		List<String> gem = new ArrayList(); 
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(pathname)));
+			for (String line = br.readLine(); line != null; line = br.readLine()) {
+//					System.out.println(line);
+					gem.add(line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+        	try {
+        		if (br != null)br.close();
+        		
+        	} catch (IOException ex) {
+        		ex.printStackTrace();
+        	}
+        }
+		for (int i=0;i<gem.size();i++)
+			if (gem.get(i).contains(name))
+				return i+1;
+		return 0;
+}
 }
