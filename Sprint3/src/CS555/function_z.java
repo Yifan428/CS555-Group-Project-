3e4554c478c7aa4aea4263c793ba5eca5f3455c1
zlaomin <zlaomamin@gmail.com>
2018-11-06 18:10:25 -0500

    Update function_z.java

--- Sprint3/src/CS555/function_z.java
+++ Sprint3/src/CS555/function_z.java
@@ -42,7 +42,7 @@ public class function_z {
 				namelist=sonname.split(" ");
 				if(namelist[namelist.length-1]!=failyname){
 					System.out.print("line "+function_z.findwhichline(namelist[namelist.length-1]));
-					System.out.println(" ERROR: FAMILY: US16: The F"+f.id+" family's man's last name is not same ");	
+					System.out.println(" ERROR: FAMILY: US16: The "+f.id+" family's man's last name is not same ");	
 					namelist[namelist.length-1]=failyname;
 				}
 				sonname="";
@@ -63,7 +63,7 @@ public class function_z {
 			return;
 		if(f.childrenname.size()>5) {
 			System.out.print("line "+ function_z.findwhichline(f.id));
-			System.out.println(" ERROR: FAMILY: US14 The F"+f.id+" family have more than 5 child");
+			System.out.println(" ERROR: FAMILY: US14 The "+f.id+" family have more than 5 child");
 	}
 }
 	public static int findwhichline(String name) {
@@ -109,7 +109,7 @@ public class function_z {
 			Family temp = f.get(i);
 			if(!function_z.Check_date(temp.marriageDate)||!function_z.Check_date(temp.divorceDate)) {
 				System.out.print("line "+ function_z.findwhichline(temp.id));
-				System.out.println(" ERROR: FAMILY: US42 The F"+temp.id+" Family have illegitimate_dates");
+				System.out.println(" ERROR: FAMILY: US42 The "+temp.id+" Family have illegitimate_dates");
 			}
 		}
 	}
@@ -125,11 +125,11 @@ public class function_z {
 			long birthdate= function_z.change_date(I.get(num-1).birthDate).getTime();
 			if(birthdate-devoce>nine) {
 				System.out.print("line "+ function_z.findwhichline(I.get(i).id));
-				System.out.println(" ERROR: FAMILY: US8 The F"+f.id+" Family have someone birth after device 9 mon");
+				System.out.println(" ERROR: FAMILY: US8 The "+f.id+" Family have someone birth after device 9 mon");
 			}
 			if(birthdate>marry) {
 				System.out.print("line "+ function_z.findwhichline(I.get(i).id));
-				System.out.println(" ERROR: FAMILY: US8 The F"+f.id+" Family have someone birth before marryage");
+				System.out.println(" ERROR: FAMILY: US8 The "+f.id+" Family have someone birth before marryage");
 			}
 		}
 	}
